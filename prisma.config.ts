fffffbe84a3dc19c42e4d3dd0c6bb1b99c4212bc
dnsxmrs <ericemarial@gmail.com>
2026-01-19 02:18:11 +0800

    feat: Update dependencies and enhance Prisma configuration for PostgreSQL

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,8 +5,12 @@ export default defineConfig({
     schema: 'prisma/schema.prisma',
     migrations: {
         path: 'prisma/migrations',
+        seed: 'tsx prisma/seed.ts',
     },
     datasource: {
+        // Use direct connection for migrations (port 5432)
+        // url: env('DIRECT_URL'),
         url: env('DATABASE_URL'),
+        // url: process.env.DATABASE_URL,
     },
 })
--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,10 +1,20 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client'
+import pg from 'pg'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+// Create PostgreSQL adapter for Prisma v7
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
 
-export { prisma }
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma
+}
